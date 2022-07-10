7e67c3838e0172912c773d493222f90e5fa98c3a
cutlets14 <cutlets14@gmail.com>
2022-07-10 12:24:50 -0400

    initial commit

---
+++ challenge_1/index.js
@@ -0,0 +1,17 @@
+// Thought experiment
+const parent = (
+    <div>
+        <img src='react_logo.png'></img>
+        <p className="paragraph">This is a sample paragraph with a className of "paragraph"</p>
+        <ul className="unordered-list">
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            <li>Item 4</li>
+        </ul>
+        <p className="paragraph2">This is another sample paragraph with a different className of "paragraph2"</p>
+    </div>
+)
+
+const rootEl = document.getElementById("root")
+ReactDOM.render(parent, rootEl)
