340580c949965ed1ba5f4e1928f016dada7ab201
cutlets14 <cutlets14@gmail.com>
2022-07-10 13:02:11 -0400

    practice with arrow components

--- custom_components/index.js
+++ custom_components/index.js
@@ -1,5 +1,20 @@
-// Thought experiment
-function TemporaryComponent() {
+// Thought experiment with custom components
+// function TemporaryComponent() {
+//     <div>
+//         <img src="./react_logo.png" id="react-logo" width="40px"></img>
+//         <h1 className="header-one">Fun facts about React</h1>
+//         <ul className="unordered-list">
+//             <li>Was first released in 2013</li>
+//             <li>Was originally created by Jordan Walke</li>
+//             <li>Has well over 100K stars on GitHub</li>
+//             <li>Is maintained by Facebook</li>
+//             <li>Powers thousands of enterprise apps, including mobile apps</li>
+//         </ul>
+//     </div>
+// }
+
+// Using arrow notation
+const TemporaryComponent = () => {
     <div>
         <img src="./react_logo.png" id="react-logo" width="40px"></img>
         <h1 className="header-one">Fun facts about React</h1>
