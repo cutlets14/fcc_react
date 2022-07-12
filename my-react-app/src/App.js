986313d3174114dcf8218839e594fc6fb5f91540
cutlets14 <cutlets14@gmail.com>
2022-07-12 19:51:32 -0400

    introduce styling to navbar

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,5 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
 import Navbar from './components/Navbar'
 import Main from './components/Main'
 
@@ -7,7 +5,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <Navbar/>
         <Main/>
       </header>
--- my-react-app/src/components/Navbar.js
+++ my-react-app/src/components/Navbar.js
@@ -1,6 +1,14 @@
 const Navbar = () => {
     return (
-        <h1>Navbar goes here</h1>
+        <nav>
+            <img src="./react_logo.png" alt="React logo" className="nav--icon"></img>
+            <h3 className="nav--logo_text">
+                ReactFacts
+            </h3>
+            <h4 className="nav--title">
+                React Course - Project 1
+            </h4>
+        </nav>
     )
 }
 
