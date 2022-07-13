9284caba7c11afeac5a7631a11467c8a6e2f4773
cutlets14 <cutlets14@gmail.com>
2022-07-13 19:25:17 -0400

    add main component and style it

--- my-react-app/src/components/Main.js
+++ my-react-app/src/components/Main.js
@@ -1,6 +1,15 @@
 const Main = () => {
     return (
-        <h1>Main goes here</h1>
+        <main>
+            <h1 className="div--header1">Fun facts about React</h1>
+            <ul className="div--unlist">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
     )
 }
 
