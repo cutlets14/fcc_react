459bb9cfe93c595c517084682888ade7658dbc14
cutlets14 <cutlets14@gmail.com>
2022-07-13 20:24:51 -0400

    broken code - argh

---
+++ digital_business_card/src/components/Main.js
@@ -0,0 +1,7 @@
+const Main = () => {
+    return (
+        <h1>The main app content goes here</h1>
+    )
+}
+
+export default Main;
---
+++ digital_business_card/src/components/Navbar.js
@@ -0,0 +1,7 @@
+const Navbar = () => {
+    return (
+        <nav>The navbar app content goes here</nav>
+    )
+}
+
+export default Navbar;
