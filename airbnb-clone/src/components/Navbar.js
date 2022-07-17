0c41e73639c28e49a19787ef13c50dd8425328ee
cutlets14 <cutlets14@gmail.com>
2022-07-17 08:46:00 -0400

    add navbar with logo

---
+++ airbnb-clone/src/components/Navbar.js
@@ -0,0 +1,9 @@
+const Navbar = () => {
+    return (
+        <nav className="topnav">
+            <img src="../airbnb-logo.svg" className="nav-logo" alt="airbnb logo"></img>
+        </nav>
+    )
+}
+
+export default Navbar;
